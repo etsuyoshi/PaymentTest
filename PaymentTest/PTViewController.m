97b6de7316c5623c264c7745b6373baa430b86c4
tendo <tuyo1009@gmail.com>
2013-12-09 19:44:28 +0900

    first commit

--- PaymentTest/PTViewController.m
+++ PaymentTest/PTViewController.m
@@ -13,28 +13,122 @@
 @end
 
 @implementation PTViewController
-
+UIActivityIndicatorView *activityIndicator;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    activityIndicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+//    activityIndicator.frame = CGRectMake(0, 0, 100, 100);
+    activityIndicator.center = CGPointMake(self.view.bounds.size.width/2,
+                                           self.view.bounds.size.height/2);
+    [self.view addSubview:activityIndicator];
+    
+//    UIButton *btProductRequest = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    UIButton *btProductRequest = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 50, 20)];
+    btProductRequest.center = CGPointMake(self.view.bounds.size.width/2, 50);
+    [btProductRequest setBackgroundColor:[UIColor grayColor]];
+    [btProductRequest addTarget:self
+                         action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btProductRequest];
 }
 
+-(void)buttonPressed:(id)sender{
+    UIButton *bt = (UIButton *)sender;
+    NSLog(@"bt=%@", bt);
+    
+    SKProductsRequest *request= [[SKProductsRequest alloc]
+                                 initWithProductIdentifiers: [NSSet setWithObject: @"coin"]];
+    request.delegate = self;
+    [request start];
+    
+    [activityIndicator startAnimating];
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response
+{
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    
+    NSArray *myProduct = response.products;
+    NSLog(@"%@",[[myProduct objectAtIndex:0] productIdentifier]);
+    
+    //Since only one product, we do not need to choose from the array. Proceed directly to payment.
+    
+    SKPayment *newPayment = [SKPayment paymentWithProduct:[myProduct objectAtIndex:0]];
+    [[SKPaymentQueue defaultQueue] addPayment:newPayment];
+    
+}
 
-- (void) completeTransaction: (SKPaymentTransaction *)transaction{
-    
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transactions
+{
+    for (SKPaymentTransaction *transaction in transactions)
+    {
+        switch (transaction.transactionState)
+        {
+            case SKPaymentTransactionStatePurchased:
+                [self completeTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateFailed:
+                [self failedTransaction:transaction];
+                break;
+            case SKPaymentTransactionStateRestored:
+                [self restoreTransaction:transaction];
+            default:
+                break;
+        }
+    }
 }
-- (void) restoreTransaction: (SKPaymentTransaction *)transaction{
+
+- (void) completeTransaction: (SKPaymentTransaction *)transaction
+{
+    NSLog(@"Transaction Completed");
+    // You can create a method to record the transaction.
+    // [self recordTransaction: transaction];
     
-}
-- (void) failedTransaction: (SKPaymentTransaction *)transaction{
+    // You should make the update to your app based on what was purchased and inform user.
+    // [self provideContent: transaction.payment.productIdentifier];
     
+    // Finally, remove the transaction from the payment queue.
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
 }
+
+- (void) restoreTransaction: (SKPaymentTransaction *)transaction
+{
+    NSLog(@"Transaction Restored");
+    // You can create a method to record the transaction.
+    // [self recordTransaction: transaction];
+    
+    // You should make the update to your app based on what was purchased and inform user.
+    // [self provideContent: transaction.payment.productIdentifier];
+    
+    // Finally, remove the transaction from the payment queue.
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
+}
+
+- (void) failedTransaction: (SKPaymentTransaction *)transaction
+{
+    [activityIndicator stopAnimating];
+    if (transaction.error.code != SKErrorPaymentCancelled)
+    {
+        // Display an error here.
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Purchase Unsuccessful"
+                                                        message:@"Your purchase failed. Please try again."
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
+    
+    // Finally, remove the transaction from the payment queue.
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
+}
+
+
 
 @end
